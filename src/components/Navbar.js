69a6177dc7ab19d075ced0d35744bedf0ee4f31d
sachin <sksachin7z2@gmail.com>
2023-01-17 01:30:27 +0530

    some fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ function Navbar({notify}) {
     <div>
 
          <nav>
-           <div className='w-[100vw] h-[10vh] flex justify-between items-center bg-gray-800 '>
+           <div className='fixed w-[100vw] h-[10vh] flex justify-between items-center bg-gray-800 '>
             <div className='flex items-center'>
               <Link to="/"> <div className='text-lg font-bold text-white px-16 md:px-7'>
                   Documentation
@@ -55,7 +55,7 @@ function Navbar({notify}) {
                 <button className='btn-primary text-white' onClick={()=>{setActive(!active)}}><svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg></button>
                </div>
            </div>{active&&<div className=' w-[100vw] bg-gray-800  '>
-          <ul className='px-3'>
+          <ul className='pt-[4rem]'>
                 <li>
                  <Link to='/'> <div className=' font-bold text-white hover:text-[#7171d5] rounded-md p-2 mx-2 cursor-pointer'>
                   Home
